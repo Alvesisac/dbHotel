f4c28c588e0c6bd8abe902152ca4a3a2b87748f1
ISAC ALVES DOS SANTOS <isac.asantos1@senacsp.edu.br>
2023-10-20 11:59:21 -0300

    Terceira versão

--- SQL-BDD.sql
+++ SQL-BDD.sql
@@ -55,13 +55,32 @@ insert into funcionarios(nomeFunc, login, senha, cargo) values ("Victoria Cardos
 
 insert into funcionarios(nomeFunc, login, senha) values ("Laura", "LauraLopes", md5("123@senac"));
 
+insert into funcionarios(nomefunc, login, senha, email, cargo) values ("Thiago Saga","ThiagoSaga06", md5("123@senac"), "ThiagoSaga06.gmail.com", "Gerência");
+
 /* Insert um novo funcionário na tabela com nome, login, senha criptografada */
 insert into funcionarios(nomeFunc, login, senha) values ("Felipe Coelho", "felipe", md5 ("123@senac")); 
+
+/**/
+select * from funcionarios where cargo = "Gerencia" order by nomeFunc asc;
 
 
 /* READ */
 /* Ler/Buscar as informações da tabela funcionários */
 select * from funcionarios;
+
+/*Buscar o ID e o nome do funcionário da tabela funcionários ordenando o nome alfabeticamente (ascendente, de A a Z)*/
+select idFunc as ID_funcionario, nomeFunc as Nome_Funcionario from funcionarios order by nomeFunc asc;
+
+/*Buscar o ID e o nome do funcionário da tabela funcionários ordenando o nome alfabeticamente (ascendente, de Z a A)*/
+select idFunc as ID_funcionario, nomeFunc as Nome_Funcionario from funcionarios order by nomeFunc desc;
+
+/* Buscar os campos ID com apelido ID_Funcionario, nomeFunc com o apelido Nome_Funcionario e cargo com o apelido Cargo_Funcionario
+ da tabela funcionarios ordenando de forma descendente (do maior ID para o menor) */
+select IdFunc as ID_Funcionario, nomeFunc as Nome_Funcionario, cargo as Cargo_Funcionario from funcionarios order by IdFunc desc;
+
+/* Buscar os campos ID com apelido ID_Funcionario, nomeFunc com o apelido Nome_Funcionario e cargo com o apelido Cargo_Funcionario
+ da tabela funcionarios ordenando de forma descendente (do maior ID para o menor) */
+select IdFunc as ID_Funcionario, nomeFunc as Nome_Funcionario, cargo as Cargo_Funcionario from funcionarios where cargo  <>'null' order by IdFunc desc;
 
 /* UPDATE */
 /* Atualizar o campo login na tabela funcionarios especificando o id */
@@ -78,4 +97,23 @@ update funcionarios set email = "LauraLopes15@gmail.com" where idFunc = 6;
 
 update funcionarios set email = "FelipeCoelho16@gmail.com" where idFunc = 8;
 
+/*DELETE*/
+Delete from funcionarios where idFunc = 8;
 
+
+create table quartos (
+	idQuarto int primary key auto_increment,
+    andar varchar(10) not null,
+    tipoQuarto varchar(50) not null,
+    ocupacaoMax int not null,
+    situacao char(3) not null,
+    nome varchar (50) not null,
+    descricao text,
+    preco decimal (10,2) not null,
+    tipoCama varchar (20),
+    varanda char(3)
+);
+
+describe quartos;
+
+alter table quartos add column numeroQuarto varchar(10) not null after andar;
